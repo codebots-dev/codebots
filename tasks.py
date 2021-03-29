fceae02f0472c576656a57e874a435e3e433e5f9
frankie <ranaudo@arch.ethz.ch>
2021-03-29 11:04:46 +0200

    manifest

--- tasks.py
+++ tasks.py
@@ -128,7 +128,8 @@ def docs(ctx, doctest=False, rebuild=False, check_links=False):
 def lint(ctx):
     """Check the consistency of coding style."""
     log.write('Running flake8 python linter...')
-    ctx.run('flake8 src')
+    opts = 'E501'
+    ctx.run('flake8 bots --ignore={}'.format(opts))
 
 
 @task()
