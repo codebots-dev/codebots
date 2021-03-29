00b3fe64a9617c10d2fae2142a9880ed80b19ae9
frankie <ranaudo@arch.ethz.ch>
2021-03-29 16:27:36 +0200

    doublequotes

--- setup.py
+++ setup.py
@@ -4,50 +4,50 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ["Click>=7.0", ]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ["pytest-runner", ]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = ["pytest>=3", ]
 
 setup(
-    name='codebots',
-    version='0.1.1',
+    name="codebots",
+    version="0.1.1",
     author="Francesco Ranaudo",
-    author_email='mycoderesults@gmail.com',
-    python_requires='>=3.5',
+    author_email="mycoderesults@gmail.com",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="collection of bots for tasks automation",
     entry_points={
-        'console_scripts': [
-            'codebots=codebots.cli:main',
+        "console_scripts": [
+            "codebots=codebots.cli:main",
         ],
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='codebots',
-    packages=find_packages(include=['codebots', 'codebots.*']),
+    keywords="codebots",
+    packages=find_packages(include=["codebots", "codebots.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/franaudo/codebots',
+    url="https://github.com/franaudo/codebots",
     zip_safe=False,
 )
