cf69110f1c7160810b4d6bf520c6a653e501a658
franaudo <ranaudo@arch.ethz.ch>
2021-04-13 10:31:31 +0200

    ssh stuff

--- src/codebots/__init__.py
+++ src/codebots/__init__.py
@@ -2,3 +2,8 @@
 __author__ = """Francesco Ranaudo"""
 __email__ = 'ranaudo@arch.ethz.ch'
 __version__ = '0.4.0'
+
+from .emailbot import *
+from .slackbot import *
+from .telebot import *
+from .sshbot import *
