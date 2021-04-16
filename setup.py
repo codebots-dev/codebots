0b4a5316aaf0a4b64375a4adb142fe55aa97b607
franaudo <ranaudo@arch.ethz.ch>
2021-04-16 09:33:18 +0200

    Bump version to 0.4.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test_requirements = ["pytest>=3", ]
 
 setup(
     name="codebots",
-    version="0.4.4",
+    version="0.4.5",
     author="Francesco Ranaudo",
     author_email="ranaudo@arch.ethz.ch",
     python_requires=">=3.5",
--- src/codebots/__init__.py
+++ src/codebots/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Francesco Ranaudo"""
 __email__ = 'francesco.ranaudo@gmail.com'
-__version__ = '0.4.4'
+__version__ = '0.4.5'
 
 import os
 
