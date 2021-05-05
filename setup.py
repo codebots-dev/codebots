c1b4af071ba59cab7b8b0387f189d2e990d5486a
Francesco Ranaudo <ranaudo@arch.ethz.ch>
2021-05-05 10:25:08 +0200

    Bump version to 0.8.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test_requirements = ["pytest>=3", ]
 
 setup(
     name="codebots",
-    version="0.8.2",
+    version="0.8.3",
     author="Francesco Ranaudo",
     author_email="ranaudo@arch.ethz.ch",
     python_requires=">=3.5",
--- src/codebots/__init__.py
+++ src/codebots/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Francesco Ranaudo"""
 __email__ = 'francesco.ranaudo@gmail.com'
-__version__ = '0.8.2'
+__version__ = '0.8.3'
 
 import os
 import json
