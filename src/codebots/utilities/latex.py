2581c53ec42f6958b086aed7b822c6bf5aedf010
Francesco Ranaudo <ranaudo@arch.ethz.ch>
2021-10-26 11:47:40 +0200

    pandoc installation windows

--- src/codebots/utilities/latex.py
+++ src/codebots/utilities/latex.py
@@ -11,14 +11,16 @@ from codebots.cli.cli import main
 
 def download(url='https://github.com/jgm/pandoc/releases/download/2.15/pandoc-2.15-windows-x86_64.msi'):
 
-    # r = requests.get(url, allow_redirects=True)
+    print('downloading pandoc 2.15')
+    temp_dir = tempfile.TemporaryDirectory()
+    setup_file_path = Path().joinpath(temp_dir.name, 'pandoc.msi')
 
-    # Streaming, so we can iterate over the response.
-    r = requests.get(url, stream=True)
+    r = requests.get(url, stream=True)  # Streaming to allow the progressbar.
     total_size_in_bytes = int(r.headers.get('content-length', 0))
     block_size = 1024  # 1 Kibibyte
     progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-    with open('C:/temp/pandoc.msi', 'wb') as f:
+
+    with open(setup_file_path, 'wb') as f:
         for data in r.iter_content(block_size):
             progress_bar.update(len(data))
             f.write(data)
@@ -26,15 +28,15 @@ def download(url='https://github.com/jgm/pandoc/releases/download/2.15/pandoc-2.
     if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
         print("ERROR, something went wrong")
 
-    # with open('C:/temp/pandoc.msi', 'wb') as f:
-    #     f.write(r.content)
-    # print(r)
+    try:
 
-    # temp_dir = tempfile.TemporaryDirectory()
-    # try:
-    #     process = subprocess.Popen(Path().joinpath(temp_dir.name, 'pandoc.msi'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
-    # except:
-    #     temp_dir.cleanup()
+        out = subprocess.run(["msiexec.exe", "/i", setup_file_path, "/QN", "/L*V",
+                             f"{temp_dir.name}\msilog.log", "STARTAPP=1", "SHOWHELP=Yes"])
+        if out.returncode == 0:
+            print(f"pandoc successfully installed")
+    except:
+        temp_dir.cleanup()
+        raise Exception("ERROR, something went wrong")
 
 
 if __name__ == "__main__":
