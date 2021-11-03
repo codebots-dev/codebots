d4dd201321b9373a4cd7f9cb6ff33de6628efde9
frankie <ranaudo@arch.ethz.ch>
2021-11-03 20:35:14 +0100

    tests

--- tests/test_bots.py
+++ tests/test_bots.py
@@ -5,36 +5,20 @@
 import pytest
 
 from click.testing import CliRunner
+import codebots.cli.cli
 
-from codebots.cli import cli
-
-
-# @pytest.fixture
-# def response():
-#     """Sample pytest fixture.
-
-#     See more at: http://doc.pytest.org/en/latest/fixture.html
-#     """
-#     # import requests
-#     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+bots = ['main',
+        'latexbot',
+        'sshbot',
+        'deploybot',
+        'drivebot',
+        'telebot',
+        'slackbot',
+        'emailbot']
 
 
-# def test_content(response):
-#     """Sample pytest test function with the pytest fixture as an argument."""
-#     # from bs4 import BeautifulSoup
-#     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-# def test_command_line_interface():
-#     """Test the CLI."""
-#     runner = CliRunner()
-#     result = runner.invoke(cli.main)
-#     assert result.exit_code == 0
-#     assert 'codebots.cli.main' in result.output
-#     help_result = runner.invoke(cli.main, ['--help'])
-#     assert help_result.exit_code == 0
-#     assert '--help  Show this message and exit.' in help_result.output
-
-
-def test_dummy():
-    assert True
+def test_bots():
+    for bot in bots:
+        runner = CliRunner()
+        result = runner.invoke(getattr(codebots.cli.cli, bot))
+        assert result.exit_code == 0
